8f231ede83167e429290004077a298158e11c608
Ian <pristianb007@gmail.com>
2023-03-14 13:01:49 +0700

    Updated home.blade using Route, Modal, & Asset (Offline Bootstrap)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,6 +68,35 @@
         </div>
 
         <br>
+
+        <!-- Button trigger modal -->
+        <div class="d-grid gap-2 col-4 mx-auto">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
         <div class="row-sm-3">
             <div class="offset-md-3 col-md-6">
                 <div class="text-center">
--- resources/views/layout/web.blade.php
+++ resources/views/layout/web.blade.php
@@ -5,15 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="{{asset('Asset/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('Asset/css/bootstrap.css')}}" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
+    <script href="{{asset('Asset/js/bootstrap.min.js')}}"></script>
+    <script href="{{asset('Asset/js/bootstrap.js')}}"></script>
     <div class="row">
         <div class="col">
             <nav class="navbar-expand-lg navbar-light bg-dark">
